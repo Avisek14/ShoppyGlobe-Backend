14b353f7684f255a0f1e8a63a69b511497ef65fa
Avisek Sahoo <sahoo143avisek@gmail.com>
2026-06-05 05:11:20 +0530

    fix: remove next parameter from mongoose pre save hook

--- src/models/User.js
+++ src/models/User.js
@@ -28,18 +28,11 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
+// Hash password before saving - using promise instead of next
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
